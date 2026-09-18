03ccf3579a9e13f0def9eb46cac28c2553e978d0
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-09-18 17:22:54 +0500

    feat: add navbar component and update Home page with logo refactor(auth): remove unused authentication components

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+
 export default function Home() {
   return (
     <main className="flex min-h-dvh items-center justify-center">
+      <Image src={assets.logo} alt="logo" height={300} width={300} />
       <p className="text-sm text-muted-foreground">DevFlow</p>
     </main>
   );
